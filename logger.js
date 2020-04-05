c0f1182aba08b0090503459ddabcd11881128349
todoroff <stoyansky@gmail.com>
2020-04-05 22:46:47 +0100

    rotate log files

--- logger.js
+++ logger.js
@@ -1,16 +1,21 @@
 const winston = require("winston");
+require("winston-daily-rotate-file");
 const path = require("path");
 
 const logger = winston.createLogger({
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  datePattern: "YYYY-MM-DD-HH",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
   transports: [
-    new winston.transports.File({
-      filename: path.resolve("logs", "error.log"),
+    new winston.transports.DailyRotateFile({
+      filename: path.resolve("logs", "error-%DATE%.log"),
       level: "error",
     }),
-    new winston.transports.File({
+    new winston.transports.DailyRotateFile({
       level: "info",
-      filename: path.resolve("logs", "combined.log"),
+      filename: path.resolve("logs", "combined-%DATE%.log"),
     }),
   ],
 });
