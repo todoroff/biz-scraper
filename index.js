6da44b4f7f404b41723eacd5693c3c1e2b02322b
todoroff <stoyansky@gmail.com>
2020-04-05 21:36:15 +0100

    top-level runcycle, main retry on failure, tweak cycle ordering

--- index.js
+++ index.js
@@ -6,30 +6,13 @@ const {
 } = require("./threads");
 const utils = require("./utils");
 
-// async function start() {
-//   try {
-//     const initThreads = await transformPages(await fetchPages());
-//     let prevThreads = initThreads;
-
-//     async function runCycle() {
-//       try {
-//         const currentThreads = await transformPages(await fetchPages());
-//         console.log("New Posts: " + calculateNewPosts(prevThreads, currentThreads));
-//         console.log(
-//           "New threads: " + getNewThreadIds(prevThreads, currentThreads).length
-//         );
-//         console.log(getNewThreadIds(prevThreads, currentThreads));
-//         prevThreads = { ...currentThreads };
-//       } catch (e) {
-//         console.log(e);
-//       }
-//     }
-
-//     setInterval(runCycle, 10000);
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
+async function runCycle(prevThreads, miliseconds) {
+  const currentThreads = transformPages(await fetchPages(miliseconds));
+  console.log("New Posts: " + calculateNewPosts(prevThreads, currentThreads));
+  console.log("New threads: " + getNewThreadIds(prevThreads, currentThreads).length);
+  console.log(getNewThreadIds(prevThreads, currentThreads));
+  return { ...currentThreads };
+}
 
 /**
  * Cycle generator
@@ -42,39 +25,35 @@ const utils = require("./utils");
  */
 
 async function* start(miliseconds) {
-  try {
-    const initThreads = transformPages(await fetchPages(miliseconds));
-    var prevThreads = initThreads;
-  } catch (e) {
-    console.log(e);
-  }
-  async function runCycle() {
-    const currentThreads = transformPages(await fetchPages(miliseconds));
-    console.log("New Posts: " + calculateNewPosts(prevThreads, currentThreads));
-    console.log(
-      "New threads: " + getNewThreadIds(prevThreads, currentThreads).length
-    );
-    console.log(getNewThreadIds(prevThreads, currentThreads));
-    prevThreads = { ...currentThreads };
-  }
-  await utils.wait(miliseconds);
+  const initThreads = transformPages(await fetchPages(miliseconds));
+  var prevThreads = initThreads;
+  var lastCycleStart = Date.now();
 
   while (true) {
+    console.log("start cycle");
+    await utils.wait(miliseconds - (Date.now() - lastCycleStart));
+    lastCycleStart = Date.now();
+
     try {
-      const beginCycle = Date.now();
-      await runCycle();
-      await utils.wait(miliseconds - (Date.now() - beginCycle));
-      yield;
+      prevThreads = await runCycle(prevThreads, miliseconds);
     } catch (e) {
       console.log(e);
     }
+
+    yield;
   }
 }
 
-async function main() {
-  for await (cycle of start(60000)) {
-    console.log("cycle");
+async function main(miliseconds, retryTimeOut) {
+  try {
+    for await (cycle of start(miliseconds)) {
+    }
+  } catch (e) {
+    console.log(e);
+    console.log(`Retry in ${retryTimeOut}ms`);
+    await utils.wait(retryTimeOut);
+    return main(miliseconds, retryTimeOut);
   }
 }
 
-main();
+main(10000, 5000);
